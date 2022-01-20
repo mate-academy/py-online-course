36bfae6a50ff5822e87fd73ad3e63cc92dad50bf
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-01-20 15:18:37 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
 class OnlineCourse:
-    # write your code here
-    pass
+
+    def __init__(self, name: str, description: str, weeks: int):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days):
+        if days % 7 == 0:
+            return days / 7
+        else:
+            return days // 7 + 1
+
+    @classmethod
+    def from_dict(cls, course_dict):
+        days = course_dict["days"]
+        return OnlineCourse(course_dict["name"],
+                            course_dict["description"],
+                            cls.days_to_weeks(days))
