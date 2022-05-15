c515e04da0e027474942272e511e607a8e62f18f
Viktoria Pavlej <viktorialondon16@gmail.com>
2022-05-15 20:26:12 +0100

    Solution_corrected

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ class OnlineCourse:
 
     @classmethod
     def from_dict(cls, course_dict):
-        return OnlineCourse(
+        return cls(
             name=course_dict["name"],
             description=course_dict["description"],
             weeks=OnlineCourse.days_to_weeks(course_dict["days"]))
