72bd29ca3caeac1c3eac22b0980226e7d7836546
MykolaIF <nick.pasko.82@gmail.com>
2022-05-18 01:51:45 +0300

    Commit for myself. I will continue at work

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name, description, weeks):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+
+course = OnlineCourse(
+    name="Python Basics",
+    description="The best course to start learning Python",
+    weeks=2,
+)
+print(course.description)
+
+"Commit for myself. I will continue at work"
