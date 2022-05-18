4893a067760fa6cea8abd0543973166be4bb1196
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-05-18 20:01:08 +0300

    'Used_enter_after_opening_paranthesis'

--- app/main.py
+++ app/main.py
@@ -14,7 +14,8 @@ class OnlineCourse:
 
     @classmethod
     def from_dict(cls, course_dict):
-        return OnlineCourse(course_dict['name'],
-                            course_dict['description'],
-                            cls.days_to_weeks(course_dict['days'])
-                            )
+        return OnlineCourse(
+            course_dict['name'],
+            course_dict['description'],
+            cls.days_to_weeks(course_dict['days'])
+        )
