f73d5d7eac2052e156011938c093d50ebcd84330
Yaroslav Havryliv <ghavryliv@gmail.com>
2022-05-18 15:40:36 +0300

    py-online-course solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,28 @@
+from math import ceil
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            name: str,
+            description: str,
+            weeks: int
+    ):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days):
+        weeks = ceil(days / 7)
+        return weeks
+
+    @classmethod
+    def from_dict(cls, course_dict):
+        weeks = cls.days_to_weeks(course_dict["days"])
+        new_class_instance = OnlineCourse(
+            course_dict["name"],
+            course_dict["description"],
+            weeks
+        )
+        return new_class_instance
