97ebd951e60fb28a64eba8bee308cc3f25be5f0f
Yaroslav Havryliv <ghavryliv@gmail.com>
2022-05-19 17:22:39 +0300

    cls.days_to_weeks replaced on classname.days_to_weeks

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ class OnlineCourse:
 
     @classmethod
     def from_dict(cls, course_dict):
-        weeks = cls.days_to_weeks(course_dict["days"])
+        weeks = OnlineCourse.days_to_weeks(course_dict["days"])
         new_class_instance = cls(
             course_dict["name"],
             course_dict["description"],
