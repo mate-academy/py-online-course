fadbd1427d33b01376fe2b5e72a7193c2f095b48
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-05-20 11:59:45 +0300

    'change_single_quotes'

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class OnlineCourse:
     @classmethod
     def from_dict(cls, course_dict):
         return cls(
-            course_dict['name'],
-            course_dict['description'],
-            cls.days_to_weeks(course_dict['days'])
+            course_dict["name"],
+            course_dict["description"],
+            OnlineCourse.days_to_weeks(course_dict["days"])
         )
