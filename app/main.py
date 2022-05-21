33d55a44f4c6bf8880432ce4232e215e5b422782
Sergey <zhyhadlo512@gmail.com>
2022-05-21 15:51:53 +0300

    'TheSolution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
+import math
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name, description, weeks):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days):
+        full_weeks = 0
+        if days <= 7:
+            return 1
+        else:
+            full_weeks = days / 7
+            return math.ceil(full_weeks)
+
+    @classmethod
+    def from_dict(cls, course_dict):
+        days = course_dict['days']
+        weeks = cls.days_to_weeks(days)
+        the_instance = OnlineCourse(course_dict['name'],
+                                    course_dict['description'], weeks)
+        return the_instance
