6154c60a9464142b6d888db213790e40dd7234af
Artem Kharchenko <darten360@gmail.com>
2022-05-25 10:40:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -19,7 +19,6 @@ class OnlineCourse:
         return (days // 7) + 1
 
     @classmethod
-    def from_dict(cls, course_dict):
-        return cls(course_dict["name"],
-                   course_dict["description"],
-                   cls.days_to_weeks(course_dict["days"]))
+    def from_dict(cls, course_dict: dict):
+        return cls(course_dict["name"], course_dict["description"],
+                   OnlineCourse.days_to_weeks(course_dict["days"]))
