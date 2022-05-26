8b115febd806c9b8fdfa409f8017a559fbf1114a
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-05-26 19:17:58 +0300

    'TheFix'

--- app/main.py
+++ app/main.py
@@ -9,17 +9,12 @@ class OnlineCourse:
 
     @staticmethod
     def days_to_weeks(days):
-        full_weeks = 0
-        if days <= 7:
-            return 1
-        else:
-            full_weeks = days / 7
-            return math.ceil(full_weeks)
+        return math.ceil(days / 7)
 
     @classmethod
     def from_dict(cls, course_dict):
-        days = course_dict['days']
-        weeks = cls.days_to_weeks(days)
-        the_instance = OnlineCourse(course_dict['name'],
-                                    course_dict['description'], weeks)
+        days = course_dict["days"]
+        weeks = OnlineCourse.days_to_weeks(days)
+        the_instance = cls(course_dict["name"],
+                           course_dict["description"], weeks)
         return the_instance
