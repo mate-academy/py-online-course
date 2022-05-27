e6133132df0357a8b1d8f02a17834b913b62b01e
Anton Lasko <anton.lasko1993@gmail.com>
2022-05-27 15:28:26 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -8,14 +8,12 @@ class OnlineCourse:
     def days_to_weeks(days: int) -> int:
         if days % 7 == 0:
             return int(days / 7)
-        else:
-            return int(days / 7) + 1
+        return int(days / 7) + 1
 
     @classmethod
     def from_dict(cls, course_dict):
         new_weeks = cls.days_to_weeks(course_dict['days'])
-        del course_dict['days']
         course_dict['weeks'] = new_weeks
         new_names = course_dict['name']
         new_description = course_dict['description']
-        return OnlineCourse(new_names, new_description, new_weeks)
+        return cls(new_names, new_description, new_weeks)
