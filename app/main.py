6c1144ae19a5824791398e5df5ec7a0cf39a4fe5
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-02 15:52:23 +0300

    Fixed mistakes

--- app/main.py
+++ app/main.py
@@ -6,13 +6,14 @@ class OnlineCourse:
         self.name = name
         self.description = description
         self.weeks = weeks
-        self.days_to_weeks(13)
 
     @classmethod
     def from_dict(cls, course_dict):
-        return cls(name=course_dict["name"],
-                   description=course_dict["description"],
-                   weeks=cls.days_to_weeks(course_dict["days"]))
+        return OnlineCourse(
+            name=course_dict["name"],
+            description=course_dict["description"],
+            weeks=OnlineCourse.days_to_weeks(course_dict["days"])
+        )
 
     @staticmethod
     def days_to_weeks(days):
