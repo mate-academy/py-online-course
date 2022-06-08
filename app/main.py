7dbdd613436fdbe09c3201ba7c6f1559f4d1dbd2
Artem Kazakov <artemkazakov947@gmail.com>
2022-06-08 16:12:31 +0300

    return OnlineCourse was replaced with return cls

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ class OnlineCourse:
 
     @classmethod
     def from_dict(cls, course_dict):
-        return OnlineCourse(
+        return cls(
             course_dict["name"],
             course_dict["description"],
             OnlineCourse.days_to_weeks(course_dict["days"])
