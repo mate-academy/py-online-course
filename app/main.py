dca6c37bc49e5735077faa6070d0bdbd9baa1a9d
Misha Hrynko <misha@mate.academy>
2022-06-22 13:13:56 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -15,8 +15,7 @@ class OnlineCourse:
         return math.ceil(days / 7)
 
     @classmethod
-    def from_dict(self, course_dict):
-        self.weeks = OnlineCourse.days_to_weeks(course_dict["days"])
-        course = OnlineCourse(
-            course_dict["name"], course_dict["description"], self.weeks)
-        return course
+    def from_dict(cls, course_dict):
+        cls.weeks = OnlineCourse.days_to_weeks(course_dict["days"])
+        res = cls(course_dict["name"], course_dict["description"], cls.weeks)
+        return res
