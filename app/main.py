6bb2ab0ffb92ea1690a08fd446989f9f196166a5
Anastasiia Shevchenko <106030737+shevchenko-anastasiia@users.noreply.github.com>
2022-07-02 14:37:15 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,10 +6,12 @@ class OnlineCourse:
 
     @staticmethod
     def days_to_weeks(days: int):
-        return days//5
+        return days // 5
 
     @classmethod
     def from_dict(cls, course_dict: dict):
         for _ in course_dict:
             new_weeks = cls.days_to_weeks(course_dict['days'])
-            return cls(course_dict["name"], course_dict["description"], new_weeks)
+            return cls(course_dict["name"], 
+                       course_dict["description"], 
+                       new_weeks)
