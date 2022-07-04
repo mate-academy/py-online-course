92664cb1a273bf6ce44bbfcba091b1c5f74ba760
Andriy Demeshko <ande777complaint@gmail.com>
2022-07-04 13:44:24 +0300

    Errors corrected

--- app/main.py
+++ app/main.py
@@ -12,6 +12,6 @@ class OnlineCourse:
 
     @classmethod
     def from_dict(cls, course_dict):
-        return OnlineCourse(name=course_dict["name"],
-                            description=course_dict["description"],
-                            weeks=cls.days_to_weeks(course_dict["days"]))
+        return cls(name=course_dict["name"],
+                   description=course_dict["description"],
+                   weeks=cls.days_to_weeks(course_dict["days"]))
