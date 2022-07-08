5c0fb9e00187cf06caa859a9eeea569daeea5b87
MIlan <milan.manoylov@gmail.com>
2022-07-08 15:28:30 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -7,13 +7,10 @@ class OnlineCourse:
 
     @classmethod
     def from_dict(cls, course_dict):
-        return OnlineCourse(name=course_dict["name"],
-                            description=course_dict["description"],
-                            weeks=cls.days_to_weeks(course_dict["days"]))
+        return cls(name=course_dict["name"],
+                   description=course_dict["description"],
+                   weeks=OnlineCourse.days_to_weeks(course_dict["days"]))
 
     @staticmethod
     def days_to_weeks(days):
-        week = days // 7
-        if days % 7 > 0:
-            week += 1
-        return week
+        return days // 7 if days % 7 == 0 else days // 7 + 1
