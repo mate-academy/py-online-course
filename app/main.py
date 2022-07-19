cdf700041fbb9a02cc477fa70cef8e2074a23239
yaroslav-demchenko <dastex1411@gmail.com>
2022-07-19 17:45:01 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,12 +4,19 @@ class OnlineCourse:
         self.description = description
         self.weeks = weeks
 
-    pass
+    @classmethod
+    def from_dict(cls, course_dict):
+        return cls(
+            course_dict["name"],
+            course_dict["description"],
+            OnlineCourse.days_to_weeks(course_dict["days"])
+        )
 
-
-course = OnlineCourse(
-    name="Python Basics",
-    description="The best course to start learning Python",
-    weeks=2,
-)
-print(course.description)
+    @staticmethod
+    def days_to_weeks(days):
+        week = 7
+        weeks = 0
+        while days > 0:
+            days -= week
+            weeks += 1
+        return weeks
