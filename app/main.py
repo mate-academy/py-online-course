48ea5f394967fda5135048191cabdee2139d77dd
AnDy <ansmbox@gmail.com>
2022-08-10 17:24:07 +0300

    No fixes, just for re-approvement

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ class OnlineCourse:
     def __init__(self, name, description, weeks):
         self.name = name
         self.description = description
+
         self.weeks = weeks
 
     @staticmethod
