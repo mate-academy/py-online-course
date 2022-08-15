690c75fb83276b35fdd0a56f27d603a22932903e
Anastasia Martyniuk <anastasia.20.12.1995@gmail.com>
2022-08-15 15:03:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ import math
 
 
 class OnlineCourse:
-    def __init__(self, name, description, weeks):
+    def __init__(self, name: str, description: str, weeks: int):
         self.name = name
         self.description = description
         self.weeks = weeks
 
     @staticmethod
-    def days_to_weeks(days):
+    def days_to_weeks(days: int):
         return math.ceil(days / 7)
 
     @classmethod
-    def from_dict(cls, course_dict):
+    def from_dict(cls, course_dict: str):
         return cls(
             name=course_dict["name"],
             description=course_dict["description"],
