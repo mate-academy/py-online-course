a7384315709c79f6a6b11c5b62fc26833f8f4edb
Alexander <alexzubkoff123@gmail.com>
2022-09-19 19:51:03 +0300

    Added class with converting days to weeks

--- app/main.py
+++ app/main.py
@@ -1,3 +1,29 @@
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            name: str,
+            description: str,
+            weeks: int
+    ):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        weeks = 1
+        if days > 7:
+            if days % 7 != 0:
+                weeks += days // 7
+            else:
+                weeks = days // 7
+        return weeks
+
+    @classmethod
+    def from_dict(cls, course_dict: dict):
+        weeks = OnlineCourse.days_to_weeks(course_dict["days"])
+        return cls(
+            name=course_dict["name"],
+            description=course_dict["description"],
+            weeks=weeks
+        )
