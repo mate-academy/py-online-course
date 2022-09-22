58c6e73da61f8c0f18db266e5df96a7e300551d3
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-09-22 21:47:53 +0300

    solution 3

--- app/main.py
+++ app/main.py
@@ -12,6 +12,6 @@ class OnlineCourse:
         return int(days / 7)
 
     @classmethod
-        def from_dict(cls, course_dict):
+    def from_dict(cls, course_dict):
         return cls(course_dict["name"], course_dict["description"],
                    cls.days_to_weeks(course_dict["days"]))
