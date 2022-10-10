2c5266e40fbf9975520ab989c46e818fddb5f5af
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2022-10-10 12:54:58 +0300

    create 'day_to_weeks'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 class OnlineCourse:
-    # write your code here
-    pass
+    def __int__(self, name, description, weeks):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @classmethod
+    def from_dict(cls, ):
+        ...
+
+    def course_dict(self, ):
+        ...
+
+    @staticmethod
+    def days_to_weeks(days):
+        result = int(days / 7 + 1) if isinstance(days / 7, float) else days / 7
+        return result
