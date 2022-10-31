ca6572235df42a4dc7c7d094cdc3d85b151510a4
6aPa6aH <6apa6ah666@gmail.com>
2022-10-31 15:30:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
+from math import ceil
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    def days_to_weeks(self: int) -> int:
+        weeks = ceil(self / 7)
+        return weeks
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> object:
+        new_dict = {}
+        for key, value in course_dict.items():
+            if key == "days":
+                new_dict["weeks"] = OnlineCourse.days_to_weeks(value)
+            elif key != "days":
+                new_dict[key] = value
+        return cls(new_dict["name"],
+                   new_dict["description"],
+                   new_dict["weeks"])
