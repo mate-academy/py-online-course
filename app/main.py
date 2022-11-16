27e9e2ca3d7d8e678a57c217a8b0ccb08029b141
Haishuk Pavlo <xomas@ukr.net>
2022-11-16 14:59:37 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+import math
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return int(math.ceil(days / 7))
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> callable:
+        return cls(
+            course_dict["name"],
+            course_dict["description"],
+            OnlineCourse.days_to_weeks(course_dict["days"])
+        )
