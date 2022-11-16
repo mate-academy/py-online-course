a5232fb9de8734614e3c98e171baa77af5ef7065
Haishuk Pavlo <xomas@ukr.net>
2022-11-16 15:22:39 +0200

    Revert "done"
    This reverts commit 27e9e2ca3d7d8e678a57c217a8b0ccb08029b141.

--- app/main.py
+++ app/main.py
@@ -1,21 +1,3 @@
-import math
-
-
 class OnlineCourse:
-
-    def __init__(self, name: str, description: str, weeks: int) -> None:
-        self.name = name
-        self.description = description
-        self.weeks = weeks
-
-    @staticmethod
-    def days_to_weeks(days: int) -> int:
-        return int(math.ceil(days / 7))
-
-    @classmethod
-    def from_dict(cls, course_dict: dict) -> callable:
-        return cls(
-            course_dict["name"],
-            course_dict["description"],
-            OnlineCourse.days_to_weeks(course_dict["days"])
-        )
+    # write your code here
+    pass
