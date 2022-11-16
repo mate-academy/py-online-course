5975889cf9b40918d9a193505c58bc84c7f79bc0
Haishuk Pavlo <xomas@ukr.net>
2022-11-16 18:21:14 +0200

    done

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class OnlineCourse:
     @classmethod
     def from_dict(cls, course_dict: dict) -> OnlineCourse:
         return cls(
-                course_dict["name"],
-                course_dict["description"],
-                OnlineCourse.days_to_weeks(course_dict["days"])
-            )
+            course_dict["name"],
+            course_dict["description"],
+            OnlineCourse.days_to_weeks(course_dict["days"])
+        )
