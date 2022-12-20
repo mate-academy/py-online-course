21f9cecf692cbf508788e5092dfcf61a7896a498
Taras Savchyn <savik1992@gmail.com>
2022-12-20 23:18:07 +0200

    bug fixed flake8

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from math import ceil
 
 
-
 class OnlineCourse:
     def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
@@ -13,7 +12,8 @@ class OnlineCourse:
     def from_dict(cls, course_dict: dict) -> OnlineCourse:
         name = course_dict["name"]
         description = course_dict["description"]
-        weeks = course_dict.get("weeks", cls.days_to_weeks(course_dict["days"]))
+        weeks = course_dict.get("weeks",
+                                cls.days_to_weeks(course_dict["days"]))
 
         return cls(name, description, weeks)
 
