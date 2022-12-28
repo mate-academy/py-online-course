9f89b519ce59ec3c543b6a08f24ff22147acdd4b
Sashkoooo <o.denysenko@hotmail.com>
2022-12-28 18:18:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+from __future__ import annotations
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+
+    def __init__(self, name: str, description: str, weeks: int):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return days // 7 + 1 if days % 7 != 0 else days // 7
+
+    @classmethod
+    def from_dict(cls, course_dict: dict):
+        cls.name = course_dict["name"]
+        cls.description = course_dict["description"]
+        cls.weeks = OnlineCourse.days_to_weeks(course_dict["days"])
+        return cls
