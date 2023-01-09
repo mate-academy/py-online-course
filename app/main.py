2f853a628c5d929a9446a0f924415963a36d2128
Kristina <tina.vasylenko@gmail.com>
2023-01-09 15:12:48 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,29 @@
+import math
+from typing import Any
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return math.ceil(days / 7)
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> Any:
+        for key, value in course_dict.items():
+            if "weeks" not in course_dict:
+                if key == "days":
+                    week_count = cls.days_to_weeks(value)
+                    return cls(
+                        name=course_dict.get("name"),
+                        description=course_dict.get("description"),
+                        weeks=week_count)
+            else:
+                return cls(
+                    name=course_dict.get("name"),
+                    description=course_dict.get("description"),
+                    weeks=course_dict.get("weeks"))
