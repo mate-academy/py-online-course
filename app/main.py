9023bc4e07f3494a9420f4439817c372ed870dbc
NVMBR13 <108105455+NVMBR13@users.noreply.github.com>
2023-03-16 03:35:45 +0200

    v1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class OnlineCourse:
-    # write your code here
-    pass
+    name: str
+    description: str
+    weeks: int
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return -(-days // 7)
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> object:
+        return cls(course_dict["name"],
+                   course_dict["description"],
+                   OnlineCourse.days_to_weeks(course_dict["days"]))
