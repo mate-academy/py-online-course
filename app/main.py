2e84ffbaa80131883849e7ebcab15cc6353cd113
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-06-07 11:49:20 +0300

    Implemented Online-Course task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
-class OnlineCourse:
-    # write your code here
-    pass
+class OnlineClass:
+    course_dict = {}
+
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        week = days // 7
+        if days % 7 > 0:
+            week += 1
+        return week
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> "OnlineClass":
+        return OnlineClass(
+            name=course_dict["name"],
+            description=course_dict["description"],
+            weeks=cls.days_to_weeks(course_dict["days"])
+        )
