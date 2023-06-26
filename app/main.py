81155be14552fd300b5459403de50ff7b0285795
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-26 12:03:41 +0300

    Fix flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
+
 class OnlineCourse:
     def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
-    
+
     @staticmethod
     def days_to_weeks(days: int) -> int:
         other = days % 7
