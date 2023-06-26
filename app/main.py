e7fcbf2cf6472d056568418ca2570d23beeea8ca
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-26 11:58:01 +0300

    Create days_to_weeks in OnlineCourse class

--- app/main.py
+++ app/main.py
@@ -3,3 +3,11 @@ class OnlineCourse:
         self.name = name
         self.description = description
         self.weeks = weeks
+    
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        other = days % 7
+        weeks = days // 7
+        if other:
+            return weeks + 1
+        return weeks
