aae6de97cbb56e8deb89b1fbbbcf8a88b17bb64a
Borys Kirichenko <boryskirichenko99@gmail.com>
2023-06-30 21:18:11 +0200

    All tests passed.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
+import math
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days):
+        weeks = math.ceil(days / 7)
+        return weeks
+
+    @classmethod
+    def from_dict(cls, course_dict):
+        days = course_dict["days"]
+        weeks = cls.days_to_weeks(days)
+        return cls(
+            course_dict["name"],
+            course_dict["description"],
+            weeks
+        )
