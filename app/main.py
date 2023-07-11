474aa44379fd0887d24377c4536dbdff31c09587
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-07-11 10:40:27 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,13 +8,15 @@ class OnlineCourse:
         self.name = name
         self.description = description
         self.weeks = weeks
+
     @staticmethod
     def days_to_weeks(days: int) -> int:
         return round(days / 7)
+    
     @classmethod
     def dict_convert(cls, course_dict: dict) -> OnlineCourse:
+        cls.course_dict = course_dict
         return cls(
             course_dict.get("name"),
             course_dict.get("description"),
-            OnlineCourse.days_to_weeks(course_dict.get("days"))
-        )
+            OnlineCourse.days_to_weeks(course_dict.get("days")))
