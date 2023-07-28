82fac9e5c48725bd37ee5c5cfe5bc86f303776cd
Ruslan <rusipbox@gmail.com>
2023-07-28 22:13:41 +0300

    Test (py-online-course)2

--- app/main.py
+++ app/main.py
@@ -9,12 +9,12 @@ class OnlineCourse:
 
     @staticmethod
     def days_to_weeks(days: int) -> int:
-        count_weeks = math.ceil(days / 7)   # Static method to convert number of days to number of weeks
+        count_weeks = math.ceil(days / 7)
         return count_weeks
 
     @classmethod
-    def from_dict(cls, course_dict: dict):
-        return cls(                            # Use dictionary values to initialize class attributes
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
+        return cls(
             name=course_dict["name"],
             description=course_dict["description"],
             weeks=cls.days_to_weeks(course_dict["days"])
