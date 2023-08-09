40f5baa9b7c8b9415e8ee8ded40de336bc940b99
Denys <deibukdenyscloud@gmail.com>
2023-08-09 18:22:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+from math import ceil
+from typing import Type, TypeVar
+
+Self = TypeVar("Self", bound="OnlineCourse")
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name: str = name
+        self.description: str = description
+        self.weeks: int = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return ceil(days / 7)
+
+    @classmethod
+    def from_dict(cls: Type[Self], course_dict: dict) -> Self:
+        weeks: int = cls.days_to_weeks(course_dict["days"])
+        return cls(course_dict["name"], course_dict["description"], weeks)
