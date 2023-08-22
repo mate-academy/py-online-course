b02baca5d4ddc93ed338c94c4af91767be2d9884
Nikolay Krasulin <krasulinphotograph@example.com>
2023-08-22 15:19:33 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
+import math
+from typing import Callable
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name, description, weeks):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int | float:
+        result = math.ceil(days / 7)
+        return result
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> Callable:
+        name = course_dict.get("name")
+        description = course_dict.get("description")
+        days = cls.days_to_weeks(course_dict.get("days"))
+
+        return cls(name, description, days)
