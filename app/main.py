5007e3771165337e60af1b661f8e39d9de7cc0dd
Nikolay Krasulin <krasulinphotograph@example.com>
2023-08-22 16:19:35 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import math
 
 
 class OnlineCourse:
-    def __init__(self, name, description, weeks):
+    def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
@@ -13,7 +13,7 @@ class OnlineCourse:
         return result
 
     @classmethod
-    def from_dict(cls, course_dict: dict):
+    def from_dict(cls, course_dict: dict) -> object:
         name = course_dict.get("name")
         description = course_dict.get("description")
         days = cls.days_to_weeks(course_dict.get("days"))
