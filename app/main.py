18723ceb59c43d768f9ebc7cfadbb0a5faade73c
Vitalii Taran <vitaliy.taran.1993@gmail.com>
2023-10-25 17:32:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -11,12 +11,7 @@ class OnlineCourse:
     @classmethod
     def from_dict(cls, course_dict: dict):
         weeks = cls.days_to_weeks(course_dict["days"])
-        return OnlineClass(
+        return cls(
             course_dict["name"],
             course_dict["description"],
             weeks)
-
-
-class OnlineClass(OnlineCourse):
-    def __init__(self, name: str, description: str, weeks: int):
-        super().__init__(name, description, weeks)
