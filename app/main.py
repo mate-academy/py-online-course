6ea16c7d232f0be4d040945786cd21e456880a25
job-artem <job.artem.2019@gmail.com>
2023-11-16 17:54:36 +0200

    fix flake8 remarks

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ from math import ceil
 
 
 class OnlineCourse:
-    def __init__(self, name: str, description: str, weeks: int):
+    def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
 
     @staticmethod
-    def days_to_weeks(days):
+    def days_to_weeks(days: int) -> int:
         return days // 7 + ceil((days % 7) / 7)
 
     @classmethod
