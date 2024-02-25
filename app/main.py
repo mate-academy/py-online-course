5b477b66adc2d1a1392d447e84c2d0c780a5dcc7
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-02-25 21:55:02 +0200

    Fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 
 class OnlineCourse:
-    def __init__(self, name: str, description: str, weeks: int):
+    def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
 
     @staticmethod
-    def days_to_weeks(days: int):
+    def days_to_weeks(days: int) -> int:
         return (days + 6) // 7
 
     @classmethod
