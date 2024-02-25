94db60f654d7a8f47ba244bbdcd8ff2ca4f7be96
Your_Name Your_Surname <your@email>
2024-02-25 22:17:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -14,4 +14,8 @@ class OnlineCourse:
 
     @classmethod
     def from_dict(cls, course_dict: dict) -> "OnlineCourse":
-        return cls(course_dict["name"], course_dict["description"], cls.days_to_weeks(course_dict["days"]))
+        return cls(
+            course_dict["name"],
+            course_dict["description"],
+            cls.days_to_weeks(course_dict["days"])
+        )
