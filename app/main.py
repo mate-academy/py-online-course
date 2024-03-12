27c93bffb01cedc26b081f01ea6832c9915a9c80
nazarkravchenko <nazarnazarik5@gmail.com>
2024-03-12 20:47:21 +0200

    check

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 class OnlineCourse:
-    def __init__(self, name: int, description: int, weeks: int) -> None:
+    def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
 
     @staticmethod
-    def days_to_weeks(days: int):
+    def days_to_weeks(days: int) -> int:
         return (days + 6) // 7
 
     @classmethod
-    def from_dict(cls, course_dict: int):
-        name = course_dict['name']
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
+        name = course_dict["name"]
         description = course_dict["description"]
         days = course_dict["days"]
         weeks = cls.days_to_weeks(days)
