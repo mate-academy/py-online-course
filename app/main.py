5db43b904790ec3465826732d431e550b928c471
NioviIki <charmniovi@tutanota.com>
2024-03-18 14:48:34 +0200

    added OnlineCourse

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
+from __future__ import annotations
+from math import ceil
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            name: str,
+            description: str,
+            weeks: int,
+            *args, **kwargs
+    ) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(day: int) -> int:
+        return ceil(day / 7)
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> OnlineCourse:
+        course_dict["weeks"] = cls.days_to_weeks(course_dict["days"])
+        return cls(**course_dict)
