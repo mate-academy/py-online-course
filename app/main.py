819b6021daa1a71c4818062999b09ea5f3320ead
maksym_korniev <maksymkorniev88@gmail.com>
2024-06-23 16:52:32 +0300

    online course task with static and class methods done✅

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        weeks = 0
+        for day in range(days):
+            if day % 7 == 0:
+                weeks += 1
+
+        return weeks
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "OnlineCourse":
+        return cls(data["name"], data["description"],
+                   cls.days_to_weeks(data["days"]))
+
+
+print(OnlineCourse.days_to_weeks(2))
+print(OnlineCourse.days_to_weeks(10))
+print(OnlineCourse.days_to_weeks(14))
+print(OnlineCourse.days_to_weeks(15))
