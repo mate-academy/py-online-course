d285a72c5ef2367351b5c6991921564a97fa0760
Illia Losiev <elijahlosiev7@gmail.com>
2024-07-28 13:35:11 +0300

    'flake8-fix'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+from __future__ import annotations
 import math
 
 
 class OnlineCourse:
-    def __init__(self, name, description, weeks):
+    def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
@@ -12,7 +13,7 @@ class OnlineCourse:
         return math.ceil(days / 7)
 
     @classmethod
-    def from_dict(cls, course_dict):
+    def from_dict(cls, course_dict: dict) -> OnlineCourse:
         name = course_dict["name"]
         description = course_dict["description"]
         days = course_dict["days"]
