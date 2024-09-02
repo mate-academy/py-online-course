cd2e9e3d9c1accfdda38b4fe09fd2f50a8939c79
Lev <lev2099@gmail.com>
2024-09-02 21:31:38 +0300

    add __init__, staticmethod, classmethod

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
 class OnlineCourse:
-    # write your code here
-    pass
+
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        days_count = days
+        count = 0
+        while days_count > 0:
+            days_count -= 7
+            count += 1
+        return count
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
+        return cls(
+            name=course_dict.get("name", ""),
+            description=course_dict.get("description", ""),
+            weeks=cls.days_to_weeks(course_dict.get("days", 0))
+        )
