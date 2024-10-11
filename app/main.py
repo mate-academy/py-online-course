4d691bc76c97bed77db0faa6a351fa855f9e0a4f
Max <mxbely98@gmail.com>
2024-10-11 13:23:08 +0300

    created OnlineCourse class

--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
 class OnlineCourse:
-    # course_dict = {}
     def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
-        # OnlineCourse.course_dict["name"] = name
-        # OnlineCourse.course_dict["description"] = description
-        # OnlineCourse.course_dict["days"] = weeks * 7
 
     @staticmethod
     def days_to_weeks(days: int) -> None:
