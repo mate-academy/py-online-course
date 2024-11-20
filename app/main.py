aa1796596715856c1d4eda3527aea5a93d1ef2a3
Liza Shulika <liza.shulika2009@gmail.com>
2024-11-20 20:43:56 +0200

    course1

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import math
 
+
 class OnlineCourse:
     def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
@@ -11,7 +12,16 @@ class OnlineCourse:
         return math.ceil(days / 7)
 
     @classmethod
-    def from_dict(cls, course_dict):
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
         week = cls.days_to_weeks(course_dict["days"])
-        return OnlineCourse(course_dict["name"], course_dict["description"], week )
+        return OnlineCourse(course_dict["name"],
+                            course_dict["description"],
+                            week)
 
+# course_dict = {
+#     "name": "Python Core",
+#     "description": "After this course you will know everything about Python",
+#     "days": 12,
+# }
+# python_course = OnlineCourse.from_dict(course_dict)
+# print(python_course.weeks)
