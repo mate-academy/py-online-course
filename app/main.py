4ded2e551fb30d6c2876f598ffe8421b474d52b2
Greshch <greshchook@gmail.com>
2024-12-12 14:48:17 +0200

    cls error

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
+from __future__ import annotations
+
+import math
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        day_in_week = 7
+        return math.ceil(days / day_in_week)
+
+    @classmethod
+    def from_dict(cls, course_dict: dict[str]) -> OnlineCourse:
+        return OnlineCourse(course_dict["name"],
+                            course_dict["description"],
+                            OnlineCourse.days_to_weeks(course_dict["days"])
+                            )
