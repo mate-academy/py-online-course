2ac7bc805b7f8476975c5b960b27a50514f21dbb
Alina Bondarenko <alinamoskovchuk18@gmail.com>
2025-01-13 17:13:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ class OnlineCourse:
 
     @staticmethod
     def days_to_weeks(days: int) -> int:
-          return (days + 6) // 7
+        return (days + 6) // 7
 
     @classmethod
-    def from_dict(cls, course_dict: dict):
+    def from_dict(cls, course_dict: dict) -> 'OnlineCourse':
         name = course_dict["name"]
         description = course_dict["description"]
         days = course_dict["days"]
