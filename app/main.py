cd74091f958abc948ee68cb440a4ab7e7b60d66d
Marek Smolana <msmolana@gmail.com>
2025-02-01 13:29:14 +0100

    draft

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
+from __future__ import annotations
+import math
+
 class OnlineCourse:
-    # write your code here
+    def days_to_weeks(days: int) -> int:
+        return math.ceil(days / 7.0)
     pass
+
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    def from_dict(cls, course: dict) -> OnlineCourse:
+        return OnlineCourse(course["name"], course["description"], cls.days_to_weeks(course["days"]))
