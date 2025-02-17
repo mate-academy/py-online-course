4379417a19cae14bb3b624eece92c39320913377
mira <mkyshchak@gmail.com>
2025-02-17 14:32:43 +0200

    fix_onl_course

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name, description, weeks):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days):
+        return (days + 6) // 7
+
+    @classmethod
+    def from_dict(cls, course_dict):
+        name = course_dict["name"]
+        description = course_dict["description"]
+        weeks = cls.days_to_weeks(course_dict["days"])
+        return cls(name, description, weeks)
