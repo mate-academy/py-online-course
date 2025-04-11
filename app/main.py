174882d4741b8f59c3eef3823bb51593843a7071
AlinaSv88 <s.syvokorovska@gmail.com>
2025-04-11 11:35:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,26 @@
+from __future__ import annotations
+import math
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        result = math.ceil(days / 7)
+        return result
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> OnlineCourse:
+
+        course_dict["weeks"] = math.ceil(course_dict["days"] / 7)
+        new_inst = cls(
+            course_dict["name"],
+            course_dict["description"],
+            course_dict["weeks"]
+        )
+        return new_inst
