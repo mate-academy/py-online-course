8dfaad647d048cc39041b1f368e3873548b3569b
kravetst <taraskravetc28@gmail.com>
2025-05-23 14:34:29 +0300

    a class with methods that convert days to weeks and create an object from a dictionaryÐ

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days) -> int:
+        return (days + 6) // 7
+
+    @classmethod
+    def from_dict(cls, course_dict):
+        name = course_dict.get('name')
+        description = course_dict.get('description')
+        days = course_dict.get('days')
+        weeks = cls.days_to_weeks(days)
+        return cls(name, description, weeks)
