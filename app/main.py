efa86173e98a6dd0abdeaf4ff85f1d014984c8a8
Mirosław Kudłacz <mirek.kudlacz@gmail.com>
2025-06-03 10:58:50 +0200

    Fixed Splution_v2

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-from typing import Any
 import math
 
 
@@ -15,7 +14,7 @@ class OnlineCourse:
         return days_to_weeks
 
     @classmethod
-    def from_dict(cls, course_dict: dict[str, Any]) -> "OnlineCourse":
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
         name = course_dict["name"]
         description = course_dict["description"]
         weeks = cls.days_to_weeks(course_dict["days"])
