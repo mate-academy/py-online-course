b699f9a2626b9430a2e86f7ec0507993297bcfbd
Paulo Rodrigues <paulo.jorge.rodrigues@gmail.com>
2025-06-13 12:48:45 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ class OnlineCourse:
 
     @classmethod
     def from_dict(cls, course_dict: dict) -> "OnlineCourse":
-        cls.__init__(course_dict["name"], 
-                     course_dict["description"], 
+        cls.__init__(course_dict["name"],
+                     course_dict["description"],
                      OnlineCourse.days_to_weeks(course_dict["days"]))
 
         return cls
