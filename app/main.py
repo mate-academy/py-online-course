341890020fce93cc16ded69c72b4e7869e70d6e6
Bohdan Yagginov <bhders62@gmail.com>
2025-06-23 15:44:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,43 @@
+from __future__ import annotations
+from typing import Self
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        weeks: int
+    ) -> None:
+        if not isinstance(name, str):
+            raise TypeError(
+                f"Unsupported type for name: {type(name).__name__}"
+            )
+        self.name = name
+
+        if not isinstance(description, str):
+            raise TypeError(
+                f"Unsupported type for"
+                f"description: {type(description).__name__}"
+            )
+        self.description = description
+
+        if not isinstance(weeks, int):
+            raise TypeError(
+                f"Unsupported type for weeks: {type(weeks).__name__}"
+            )
+        self.weeks = weeks
+
+    @classmethod
+    def days_to_weeks(cls, days: int) -> int:
+        if not isinstance(days, int):
+            raise TypeError(f"Unsupported type: {type(days).__name__}")
+        return days // 7 + (1 if days % 7 != 0 else 0)
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> Self:
+        return cls(
+            course_dict.get("name", ""),
+            course_dict.get("description", ""),
+            cls.days_to_weeks(course_dict.get("days", 0))
+        )
