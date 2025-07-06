4f100afebac28fc518a76e3045780659af9fb6ad
Evgeniya0208 <evgeniya.tyutyunik@gmail.com>
2025-07-06 17:41:15 +0300

    Revert "Solution"
    This reverts commit 9041f0d15dbc7b32e66303458f8253b3b9a3981c.

--- app/main.py
+++ app/main.py
@@ -1,20 +1,3 @@
-import math
-from typing import Self
-
-
 class OnlineCourse:
-    def __init__(self, name: str, description: str, weeks: int) -> None:
-        self.name = name
-        self.description = description
-        self.weeks = weeks
-
-    @staticmethod
-    def days_to_weeks(days: int) -> int:
-        return math.ceil(days / 7)
-
-    @classmethod
-    def from_dict(cls, course_dict: dict) -> Self:
-        weeks = cls.days_to_weeks(course_dict.get("days"))
-        description = course_dict.get("description")
-        name = course_dict.get("name")
-        return cls(name, description, weeks)
+    # write your code here
+    pass
