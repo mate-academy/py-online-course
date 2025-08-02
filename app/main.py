cce97bdc106c36aecb3a503699324baf1ae467e7
Oleh Halas <oleggalas8@gmail.com>
2025-08-02 14:18:00 +0300

    Imlement class OnlineCourse

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+import math
+
 class OnlineCourse:
-    # write your code here
-    pass
+
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+    @staticmethod
+    def days_to_week(days: int) -> int:
+        return math.ceil(days / 7)
+    def from_dict(cls, course_dict: dict):
+        name = course_dict["name"]
+        description = course_dict["description"]
+        days = course_dict["days"]
+        weeks = OnlineCourse.days_to_week(days)
+        return cls(name, description, weeks)
