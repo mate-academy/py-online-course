683aee3fbfedfe5880a90d66d692fb84c0753787
Hunach <sasha2001sek@gmail.com>
2025-08-13 15:51:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+from typing import Dict
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @classmethod
+    def from_dict(cls, course_dict: Dict) -> "OnlineCourse":
+        name = course_dict.get("name", "")
+        description = course_dict.get("description", "")
+        days = course_dict.get("days")
+        weeks = cls.days_to_weeks(days)
+        return cls(name=name, description=description, weeks=weeks)
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return (days + 6) // 7
