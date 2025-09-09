246a87c4cea694f5bdce2e21db5a9de0ea517cfc
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-09 21:03:55 +0300

    fix a problem4

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 class OnlineCourse:
     def __init__(self, name: str, description: str, weeks: int) -> None:
-        self.name = name
-        self.description = description
-        self.weeks = weeks
+        self.name: str = name
+        self.description: str = description
+        self.weeks: int = weeks
 
     @staticmethod
     def days_to_weeks(days: int) -> int:
-        # Округлення вгору без math.ceil
+        # округлення вгору без імпортів
         return (days + 6) // 7
 
     @classmethod
-    def from_dict(cls, course_dict) -> "OnlineCourse":
-        name = course_dict["name"]
-        description = course_dict["description"]
-        weeks = cls.days_to_weeks(course_dict["days"])
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
+        name: str = course_dict["name"]
+        description: str = course_dict["description"]
+        weeks: int = cls.days_to_weeks(course_dict["days"])
         return cls(name, description, weeks)
