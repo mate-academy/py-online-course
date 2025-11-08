35256d103049e7b8d0a98f6efdf83634c5754061
Bohdan Soloviov <bogdan.solovev.02@gmail.com>
2025-11-08 13:17:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
+import math
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return math.ceil(days / 7)
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
+        required = ["name", "description", "days"]
+        for key in required:
+            if key not in course_dict:
+                raise KeyError(f"Missing key {key}")
+        name = course_dict["name"]
+        description = course_dict["description"]
+        weeks = cls.days_to_weeks(course_dict["days"])
+        return cls(name, description, weeks)
