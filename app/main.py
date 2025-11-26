5f32a123a8010e761be553f7d05bca438356490e
Mykola Mosiienko <koljamos@gmail.com>
2025-11-26 10:52:45 +0200

    done W293 blank line contains whitespace

--- app/main.py
+++ app/main.py
@@ -5,15 +5,15 @@ class OnlineCourse:
     def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
-        self.weeks = weeks
-        
+        self.weeks = weeks    
+
     @staticmethod
     def days_to_weeks(days: int) -> int:
         weeks = days // 7
         if days % 7 != 0:
             weeks += 1
         return weeks
-    
+
     @classmethod
     def from_days(cls, course_dict: dict) -> OnlineCourse:
         name = course_dict.get("name")
