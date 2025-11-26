55640b9af0ce3a52625203fef34557dbc7a2cef5
Mykola Mosiienko <koljamos@gmail.com>
2025-11-26 10:41:08 +0200

    Implemented Class OnlineCourse

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+from __future__ import annotations
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+        
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        weeks = days // 7
+        if days % 7 != 0:
+            weeks += 1
+        return weeks
+    
+    @classmethod
+    def from_days(cls, course_dict: dict) -> OnlineCourse:
+        name = course_dict.get('name')
+        description = course_dict.get('description')
+        days = course_dict.get('days')
+        weeks = cls.days_to_weeks(days)
+        return cls(name, description, weeks)
