7254eaaf83966ac5ced92b1d45abe1d98f0fa106
Mykola Mosiienko <koljamos@gmail.com>
2025-11-26 10:54:25 +0200

    again app/main.py:8:27: W291 trailing whitespace

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class OnlineCourse:
     def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
-        self.weeks = weeks    
+        self.weeks = weeks
 
     @staticmethod
     def days_to_weeks(days: int) -> int:
