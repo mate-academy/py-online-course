0d7d608a6be4ffa93a05b1d5a06d4c5e58d05ffb
Artem <artem90078@gmail.com>
2025-11-29 17:28:36 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import math
 
 
 class OnlineCourse:
-    def __init__(self, name: str, description: str, weeks: int) -> None: # Fix 1
+    def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
@@ -14,14 +14,13 @@ class OnlineCourse:
         return math.ceil(days / 7)
 
     @classmethod
-    def from_dict(cls, course_dict: dict) -> 'OnlineCourse': # Fix 2
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
         course_name = course_dict["name"]
         course_description = course_dict["description"]
         course_days = course_dict["days"]
 
         course_weeks = cls.days_to_weeks(course_days)
 
-        # Fix 3 (Line broken up)
         return cls(
             name=course_name,
             description=course_description,
