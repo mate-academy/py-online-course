9594d4f0d4cf701a4a1227edc6ff5d79a8ee8003
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2025-12-03 12:37:18 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        # 1 week = 7 days
+        return (days + 6) // 7
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
+        name = course_dict["name"]
+        description = course_dict["description"]
+        days = course_dict["days"]
+        weeks = cls.days_to_weeks(days)
+        return cls(name, description, weeks)
