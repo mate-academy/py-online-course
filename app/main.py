bdd9850670a49ac79bb8fc16a91a7291a84336b2
John McLeish <mcleish.john@gmail.com>
2025-12-28 12:00:23 +1100

    Stood up OnlineCourse class, untested

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+import math
+from __future__ import annotations
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+    
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return math.ceil(days / 7)
+    
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> OnlineCourse:
+        name = course_dict.get('name', 'Unnamed Course')
+        description = course_dict.get('description', '')
+        weeks = cls.days_to_weeks(course_dict.get('days', 0))
+        return cls(name, description, weeks)
