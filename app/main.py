dbdd8a0d9dffdba8abb03fbfb8e412512bc6c1c5
Viesich <v.s.vesich@gmail.com>
2026-01-10 16:00:07 +0200

    fix flake8

--- app/main.py
+++ app/main.py
@@ -3,7 +3,12 @@ from typing import Any
 
 
 class OnlineCourse:
-    def __init__(self, name: str, description: str, weeks: int) -> None:
+    def __init__(
+            self,
+            name: str,
+            description: str,
+            weeks: int
+    ) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
