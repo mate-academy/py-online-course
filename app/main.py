548fb97e8ca22b41d4d9bf4775fa85e4327faee6
Viesich <v.s.vesich@gmail.com>
2026-01-10 15:40:41 +0200

    fix main.py

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ class OnlineCourse:
 
     @classmethod
     def from_dict(cls, course_dict: dict[str, Any]) -> OnlineCourse:
-        cls.name = course_dict["name"]
-        cls.description = course_dict["description"]
-        cls.weeks = cls.days_to_weeks(course_dict["days"])
-        return cls(name=cls.name, description=cls.description, weeks=cls.weeks)
+        return cls(
+            name=course_dict["name"],
+            description=course_dict["description"],
+            weeks=cls.days_to_weeks(course_dict["days"])
+        )
