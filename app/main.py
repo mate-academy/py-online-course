b0ef57389f485e79391bf9023870a840e371707d
Melon <ribudidze@gmail.com>
2026-01-24 15:07:20 +0200

    solution #1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
+import math
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        if isinstance(days, int) and days > 0:
+            weeks = days / 7
+            return math.ceil(weeks)
+        raise TypeError("Days must be an integer!")
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
+        name = course_dict.get("name")
+        description = course_dict.get("description")
+        days = course_dict.get("days")
+        weeks = cls.days_to_weeks(days)
+        return cls(name, description, weeks)
