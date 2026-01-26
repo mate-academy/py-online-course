5f69ea1133b70d832c7815357e0f646751906c52
madlings <nagalingam.s@gmail.com>
2026-01-26 22:53:07 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,15 +3,16 @@ import math
 class OnlineCourse:
 
     @classmethod
-    def from_dict(cls, course_dict):
-    	return OnlineCourse(course_dict["name"], course_dict["description"],
-        cls.days_to_weeks(course_dict["days"]))
-    
+    def from_dict(cls, course_dict: dict) -> cls:
+    	return OnlineCourse(course_dict["name"], course_dict["description"], 
+                            cls.days_to_weeks(course_dict["days"]))
+
     @staticmethod
     def days_to_weeks(days: int) -> int:
-    	return math.ceil(days / 7)
-    
-    def __init__(self, name:str, description:str, weeks:int):
+        return math.ceil(days / 7)
+
+    def __init__(self, name: str, description: str, weeks: int):
         self.name = name
         self.description = description
         self.weeks = weeks
+        
