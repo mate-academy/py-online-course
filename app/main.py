5630782d1c2a7047e682f2fc034510b19d0e4288
K N <niedbalaproject@gmail.com>
2026-02-08 22:37:51 +0100

    feat: create OnlineCourse class supporting creation from weeks and from dictionary

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
+from __future__ import annotations
+from typing import Dict
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name: str = name
+        self.description: str = description
+        self.weeks: int = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return (days + 6) // 7
+
+    @classmethod
+    def from_dict(cls, course_dict: Dict[str, str | int]) -> OnlineCourse:
+        name = course_dict["name"]
+        description = course_dict["description"]
+        days = course_dict["days"]
+
+        weeks = cls.days_to_weeks(days)
+
+        return cls(name, description, weeks)
