942e78ff79b6c99ee48ef0ae21e552c946281786
Karol <karolmarkut11@gmail.com>
2026-02-16 14:44:34 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
+import math
+
+
 class OnlineCourse:
     def __init__(self, name:str, description:str, weeks:int):
         self.name = name
         self.description = description
         self.weeks = weeks
+    @staticmethod
+    def days_to_weeks(days):
+        return math.ceil(days / 7)
+
