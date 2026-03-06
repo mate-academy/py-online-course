26ad9eef0634ffb48e5d0a81444f2e89d0f90cca
Igor <zalugin.iv2@gmail.com>
2026-03-06 14:17:31 +0100

    small flake8 changes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import math
+
+
 class OnlineCourse:
 
     def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
-
 
     @staticmethod
     def days_to_weeks(days: int) -> int:
