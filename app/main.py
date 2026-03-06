a9b82757b759e69ab3d95663050c9474e8726b17
Igor <zalugin.iv2@gmail.com>
2026-03-06 14:04:06 +0100

    small flake8 changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import math
 class OnlineCourse:
 
-    def __init__(self, name, description, weeks) -> None:
+    def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
@@ -9,7 +9,6 @@ class OnlineCourse:
     @staticmethod
     def days_to_weeks(days: int) -> int:
         return math.ceil(days / 7)
-
 
     @classmethod
     def from_dict(cls, course_dict: dict) -> OnlineCourse:
