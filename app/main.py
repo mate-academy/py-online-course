d98bcc029c5273dfd9061a34860b3d7bd9f47383
Дмитро Кедьо <your@mail.com>
2026-03-25 15:51:25 +0200

    fix: add type hints to OnlineCourse class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
+import math
+from typing import Dict
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return math.ceil(days / 7)
+
+    @classmethod
+    def from_dict(cls, course_dict: Dict[str, object]) -> "OnlineCourse":
+        name = course_dict["name"]
+        description = course_dict["description"]
+        days = course_dict["days"]
+
+        weeks = cls.days_to_weeks(days)  # type: ignore
+
+        return cls(name, description, weeks)
