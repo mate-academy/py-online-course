263eeb6f722e59706eb2222e60c2d6e9c72c11c6
Urszula <marciszewska.u@gmail.com>
2026-03-25 14:52:20 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import math
 
+
 class OnlineCourse:
     def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
@@ -16,4 +17,3 @@ class OnlineCourse:
         description = course_dict["description"]
         weeks = cls.days_to_weeks(course_dict["days"])
         return cls(name=name, description=description, weeks=weeks)
-
