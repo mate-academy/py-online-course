83c068f63b97772fda411b8e33d277ceaecccc93
Karolina Lewszyk <k.lewszyk1@gmail.com>
2026-04-09 10:39:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
+from __future__ import annotations
+import math
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        number_of_weeks = math.ceil(days / 7)
+        return number_of_weeks
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
+        course_data = {}
+        for key, value in course_dict.items():
+            if key == "days":
+                course_data["weeks"] = cls.days_to_weeks(value)
+            elif key == "name":
+                course_data["name"] = value
+            elif key == "description":
+                course_data["description"] = value
+        return cls(**course_data)
