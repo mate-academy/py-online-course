577ac9762b0365103831ee65c1e6e17297608d14
Gena <genada89@gmail.com>
2026-04-15 14:40:03 +0300

    create class OnlineCourse

--- app/main.py
+++ app/main.py
@@ -1,3 +1,29 @@
+import math
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        return math.ceil(days / 7)
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
+        name = course_dict["name"]
+        description = course_dict["description"]
+        weeks = cls.days_to_weeks(course_dict["days"])
+        return cls(name, description, weeks)
+
+
+course_dict = {
+    "name": "Python Core",
+    "description": "After this course you will know everything about Python",
+    "days": 12,
+}
+
+python_course = OnlineCourse.from_dict(course_dict)
+print(python_course.weeks)
