832f30cf854c8ecf1f5603f19fffacd47cc67a3c
Dima-shima <shifrin1970@gmail.com>
2026-05-07 15:35:21 +0300

    implement py-online-course

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days: int) -> int:
+        if (days / 7) < 1:
+            return 1
+        if days % 7:
+            return int(days / 7) + 1
+        return round(days / 7)
+
+    @classmethod
+    def from_dict(cls, course_dict: dict) -> None:
+        course = cls(course_dict["name"], course_dict["description"], course_dict["days"])
+        course.weeks = course.days_to_weeks(course_dict["days"])
+        return course
