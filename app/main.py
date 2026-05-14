bc813a39abc7f5f178bfd2aaaf5285c0b1b91c5a
Mirash1l1 <killer28392@gmail.com>
2026-05-14 18:37:41 +0300

    Сделано задние № 2

--- app/main.py
+++ app/main.py
@@ -1,20 +1,30 @@
+from __future__ import annotations
+
 from math import ceil
 
 
 class OnlineCourse:
-    def __init__(self, name, description, weeks):
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        weeks: int
+    ) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
 
     @staticmethod
-    def days_to_weeks(days):
+    def days_to_weeks(days: int) -> int:
         return ceil(days / 7)
 
     @classmethod
-    def from_dict(cls, course_dict):
+    def from_dict(
+        cls,
+        course_dict: dict
+    ) -> OnlineCourse:
         return cls(
-            course_dict["name"],
-            course_dict["description"],
-            cls.days_to_weeks(course_dict["days"])
+            name=course_dict["name"],
+            description=course_dict["description"],
+            weeks=cls.days_to_weeks(course_dict["days"])
         )
