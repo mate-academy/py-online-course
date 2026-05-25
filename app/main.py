693a4ffbe789d0eced14df4789b5c799e7a8ead4
askawa <ask_awa@icloud.com>
2026-05-25 15:07:17 +0300

    type annoteted dict

--- app/main.py
+++ app/main.py
@@ -13,9 +13,9 @@ class OnlineCourse:
         return weeks
 
     @classmethod
-    def from_dict(cls, course_dict) -> object:
+    def from_dict(cls, course_dict: dict) -> object:
         name = course_dict.get("name")
         descr = course_dict.get("description")
         days = course_dict.get("days")
         weeks = cls.days_to_weeks(days=days)
-        return cls(name, descr, weeks)
+        return cls(name=name, description=descr, weeks=weeks)
