356aaffa590964cf5e0cc4b9281caa4a37db1814
Vitalii <stalchenkovitaliy@gmail.com>
2026-06-06 21:36:05 +0400

    Start solving OnlineCourse class task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name, description, weeks):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days):
+        result = days // 7
+        if days % 7 > 0:
+            result += 1
+        return result
+
+    def from_dict(self):
