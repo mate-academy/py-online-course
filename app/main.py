3025acac44154ead72ffc066219bdf6385bb5dc0
Vitalii <stalchenkovitaliy@gmail.com>
2026-06-08 11:48:37 +0400

    Solve Online Course task

--- app/main.py
+++ app/main.py
@@ -1,14 +1,30 @@
 class OnlineCourse:
-    def __init__(self, name, description, weeks):
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        weeks: int
+    ) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
 
     @staticmethod
-    def days_to_weeks(days):
+    def days_to_weeks(days: int) -> int:
         result = days // 7
+
         if days % 7 > 0:
             result += 1
+
         return result
 
-    def from_dict(self):
+    @classmethod
+    def from_dict(
+        cls,
+        course_dict: dict
+    ) -> "OnlineCourse":
+        name = course_dict["name"]
+        description = course_dict["description"]
+        weeks = cls.days_to_weeks(course_dict["days"])
+
+        return cls(name, description, weeks)
