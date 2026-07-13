3e5e20e2d12590d085f3074dbf78b976df20de63
Nikita <nikita08102006r@gmail.com>
2026-07-13 19:55:09 +0300

    Task fix

--- app/main.py
+++ app/main.py
@@ -1,19 +1,16 @@
 class OnlineCourse:
 
-    def __init__(self, name, description, weeks):
+    def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
 
     @staticmethod
-    def days_to_weeks(day):
+    def days_to_weeks(day: int) -> int:
         week = (day + 6) // 7
         return week
 
     @classmethod
-    def from_dict(cls, course_dict):
-        name = course_dict["name"]
-        description = course_dict["description"]
-        day = course_dict["days"]
-        weeks = cls.days_to_weeks(day)
-        return cls(name, description, weeks)
+    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
+        weeks = cls.days_to_weeks(course_dict["days"])
+        return cls(course_dict["name"], course_dict["description"], weeks)
