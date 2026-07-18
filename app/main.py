dd9fa540733e3d0acc469ccf8015f5ac072e55d0
Vasyl Perehinets <perehinets95@gmail.com>
2026-07-18 19:39:39 +0100

    new Solution

--- app/main.py
+++ app/main.py
@@ -2,9 +2,7 @@ import math
 
 
 class OnlineCourse:
-    def __init__(self, name: str,
-                 description: str,
-                     weeks: int) -> None:
+    def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
